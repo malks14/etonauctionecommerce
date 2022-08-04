97e9cc21b214d38f09c5b9a31965552fd60d88cc
malks14 <73920001+malks14@users.noreply.github.com>
2022-08-04 15:38:16 -0300

    remove placeholder square

--- components/NFT/NFTCarroussel.js
+++ components/NFT/NFTCarroussel.js
@@ -1,27 +1,14 @@
-import React, { useState } from 'react'
-// import Carousel, { CarouselItem } from "../_App/Carousel";
-import Collection from '../_App/Collection'
+import React from 'react'
 import "../_App/Carousel.module.css"
 
 const NFTCarroussel = () => {
   return (
     <div className="nftCtn">
-      {/* <Carousel>
-        {collectionList.map((item, index) => {
-          return (
-            <CarouselItem key={index}>
-              <Collection
-                image={item.image}
-                collection={item.collection}
-                year={item.year}
-                artist={item.artist}  
-                chain={item.chain}
-              />
-            </CarouselItem>
-          );
-        })}
-      </Carousel> */}
-      <div style={{width: "280px", height:"340px", backgroundColor:"aqua", marginRight: "100px"}}>temporario</div>
+      {collectionList?.map((collection, index) => {
+        return (
+          <img style={{width:'280px', height:'340px', marginRight: "100px"}} key={index} src={collection.image} alt={collection.collection}/>
+        )
+      })}
           
       <div className="nftCtn_info">
 
@@ -126,46 +113,6 @@ export const CarouselItem = ({children, width}) => {
   )
 }
 
-export function Carousel({children}) {
-  const [activeIndex, setActiveIndex] = useState();
-
-  const updateIndex = (newIndex) => {
-      if(newIndex < 0) {
-          newIndex = 0;
-      } else if (newIndex >= React.Children.count(children)) {
-          newIndex = React.Children.count(children) - 1;
-      }
-
-      setActiveIndex(newIndex);
-  }
-
-  return (
-      <div className="carousel-wrapper">
-          <div className="inner" style={{transform: `translateX(-${activeIndex * 100}%)`}}>
-              {
-                  React.Children.map(children, (child, index) => {
-                      return React.cloneElement(child, {width: "100%"});
-                  })
-              }
-          </div>
-          <div className="indicators">
-              <button onClick={() => {updateIndex(activeIndex - 1 )}}> <img alt="Eton Auctions" src="/assets/arrow-left.svg"/> </button>
-              {React.Children.map(children, (child, index) => {
-                  return  (
-                      <button 
-                      className={`${index === activeIndex ? "active" : ""}`}
-                      onClick={() => {updateIndex(index)}}>
-                          {index + 1}
-                      </button>
-                  )
-              })}
-              <button onClick={() => {updateIndex(activeIndex + 1 )}}> <img alt="Eton Auctions" src="/assets/arrow-right.svg"/>  </button>
-          </div>
-      </div>
-  
-  )
-}
-
 let collectionList = [
   {
       image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634317353/etonauctions/APE_WARRIORS_1_rwz9wv.png",
@@ -176,44 +123,44 @@ let collectionList = [
       hover: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634164937/etonauctions/eactotts_lq_2_xk8uv9.jpg",
       link: "/",
   },
-  {
-      image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1637078142/etonauctions/Collections/M.%20F.%20Hussain/M-F-Hussain-EDITS_0007_20211116_113245_j4kyuu.png",
-      collection: "The Picasso of India",
-      year: "1978",
-      artist: "M. F. Husain",  
-      chain: "Polygon",
-      link: "/",
-  },
-  {
-      image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1636410301/etonauctions/Captura_de_Pantalla_2021-11-08_a_la_s_19.07.47_f7n38k.png",
-      collection: "T. Wong Collection",
-      year: "1994",
-      artist: "T. Wong",  
-      chain: "Polygon",
-      link: "/",
-  },
-  {
-      image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634316302/etonauctions/shikha_bnaqsj.png",
-      collection: "Life Doodle",
-      year: "2021",
-      artist: "Shikha Kashyap",  
-      chain: "Polygon",
-      link: "/",
-  },
-  {
-      image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634168196/etonauctions/6179bOX4QXL._AC_SL1200__qh5ehp.jpg",
-      collection: "Casual Dog Prints",
-      year: "1989",
-      artist: "Original",  
-      chain: "Polygon",
-      link: "/",
-  },
-  {
-      image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1633041442/etonauctions/Doodlecraze_-_Justin_Draws_iv1dru.png",
-      collection: "Into The Mind of Doodle",
-      year: "2021",
-      artist: "Justin Draws",  
-      chain: "Polygon",
-      link: "/",
-  },
+  // {
+  //     image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1637078142/etonauctions/Collections/M.%20F.%20Hussain/M-F-Hussain-EDITS_0007_20211116_113245_j4kyuu.png",
+  //     collection: "The Picasso of India",
+  //     year: "1978",
+  //     artist: "M. F. Husain",  
+  //     chain: "Polygon",
+  //     link: "/",
+  // },
+  // {
+  //     image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1636410301/etonauctions/Captura_de_Pantalla_2021-11-08_a_la_s_19.07.47_f7n38k.png",
+  //     collection: "T. Wong Collection",
+  //     year: "1994",
+  //     artist: "T. Wong",  
+  //     chain: "Polygon",
+  //     link: "/",
+  // },
+  // {
+  //     image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634316302/etonauctions/shikha_bnaqsj.png",
+  //     collection: "Life Doodle",
+  //     year: "2021",
+  //     artist: "Shikha Kashyap",  
+  //     chain: "Polygon",
+  //     link: "/",
+  // },
+  // {
+  //     image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1634168196/etonauctions/6179bOX4QXL._AC_SL1200__qh5ehp.jpg",
+  //     collection: "Casual Dog Prints",
+  //     year: "1989",
+  //     artist: "Original",  
+  //     chain: "Polygon",
+  //     link: "/",
+  // },
+  // {
+  //     image: "https://res.cloudinary.com/devops-imgcloud/image/upload/v1633041442/etonauctions/Doodlecraze_-_Justin_Draws_iv1dru.png",
+  //     collection: "Into The Mind of Doodle",
+  //     year: "2021",
+  //     artist: "Justin Draws",  
+  //     chain: "Polygon",
+  //     link: "/",
+  // },
 ]
